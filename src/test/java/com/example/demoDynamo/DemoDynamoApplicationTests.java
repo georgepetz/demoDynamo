ccd49c2f9d47bd3972607021899e0f8c5a2674eb
George Petz <petz.george@gmail.com>
2021-03-15 20:59:43 -0300

    First commit

---
+++ src/test/java/com/example/demoDynamo/DemoDynamoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoDynamo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDynamoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
