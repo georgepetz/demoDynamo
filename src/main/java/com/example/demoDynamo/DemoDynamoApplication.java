d611d5c777f2e6c8e86dec7445f97b36e804b537
George Petz <petz.george@gmail.com>
2021-03-15 21:22:09 -0300

    controller movies

--- src/main/java/com/example/demoDynamo/DemoDynamoApplication.java
+++ src/main/java/com/example/demoDynamo/DemoDynamoApplication.java
@@ -8,6 +8,7 @@ public class DemoDynamoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoDynamoApplication.class, args);
+
 	}
 
 }
---
+++ src/main/java/com/example/demoDynamo/controller/MoviesController.java
@@ -0,0 +1,20 @@
+package com.example.demoDynamo.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MoviesController {
+
+    @GetMapping("/list")
+    public String listMovies() {
+        return "Olá!";
+    }
+
+}
